c9d303ba6418d2e2c9f72ffa4b76142a34b92980
david hernandez <davidfelipehernandez1@gmail.com>
2022-06-08 13:19:19 -0500

    APIsDog(#8)

--- DogsAPI/script.js
+++ DogsAPI/script.js
@@ -1,23 +1,45 @@
-const dogImg = document.getElementsByClassName('dog');
+const main = document.querySelector('div');
+const imgDiv = document.createElement('div');
 
-console.log('fetchin dog img');
+//fetching the API
+async function callDogAPI() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const response2 = await fetch('https://dog.ceo/api/breeds/image/random');
+        
+        const img = await response.json();
+        const img2 = await response2.json();
 
-callDogAPI().catch(err => {
-    console.log('error');
-    console.error(err);
+        const url = img.message;
+        const url2 = img2.message;
+        
+        imgDiv.innerHTML = make(url, url2);
+
+        main.append(imgDiv);
+    } catch (error) {
+        console.log('error');
+        console.error(err);
+    }
+}
+
+//button logic
+const btn = document.createElement('button');
+btn.setAttribute('style', 'cursor: pointer')
+btn.textContent = 'Update';
+
+main.append(btn);
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    callDogAPI();
 });
 
-//more cleaner way to fetch()
-async function callDogAPI() {
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
-    const response2 = await fetch('https://dog.ceo/api/breeds/image/random');
-    
-    const img = await response.json();
-    const img2 = await response2.json();
-    
-    const url = img.message;
-    const url2 = img2.message;
-    
-    document.getElementById('dog').src = url;
-    document.getElementById('dog2').src = url2;
-}
+//Img template literal
+const make = (img1, img2) => {
+    const imgContainer = `
+    <img id="img1" src="${img1}"></img>
+    <img id="img2" src="${img2}"></img>
+    `;
+
+    return imgContainer;
+};
