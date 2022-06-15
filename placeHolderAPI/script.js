1070bdfd8678604a46dc24594ccba70b1abc048b
david hernandez <davidfelipehernandez1@gmail.com>
2022-06-15 12:38:18 -0500

    APIsPlaceHolder(#11 Format2)

--- placeHolderAPI/script.js
+++ placeHolderAPI/script.js
@@ -2,19 +2,21 @@ const app = document.getElementById('app');
 const idUser = document.getElementById('idUser');
 const ul = document.createElement('ul');
 
+//Using the API
 async function getUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const getData = await response.json();
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const getData = await response.json();
 
-    getDataP(getData);
+        getDataP(getData);
+    } catch (error) {
+        console.log('Error bbsita');
+        console.error(error);
+    }
 }
+getUsers();
 
-getUsers()
-    .catch(err => {
-        console.log('Error bbsita')
-        console.error(err)
-    })
-
+//Create the list of the users
 const getDataP = (getData) => {
     for (let i = 0; i < getData.length; i++) {
         const ul = document.createElement('ul');
@@ -23,10 +25,8 @@ const getDataP = (getData) => {
             <li>user-id: ${getData[i].name}</li>
             <li>user-id: ${getData[i].email}</li>
             <li>user-id: ${getData[i].phone}</li>
-        `
+        `;
         ul.innerHTML = templateList;
         app.append(ul);
     }
-
-    console.log(app);
-}
+};
